abe59a2c688f5c866e104c186696d3fe43be900e
shariaralphabyte <shariar@alphabytetech.com>
2025-04-20 07:07:33 +0100

    Initial Commit

---
+++ practice2/practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  practice2App.swift
+//  practice2
+//
+//  Created by MA Kader on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ practice2Tests/practice2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  practice2Tests.swift
+//  practice2Tests
+//
+//  Created by MA Kader on 20/04/2025.
+//
+
+import Testing
+@testable import practice2
+
+struct practice2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
